93f446db9012525bc2c66c835ac3d70893e706fe
Raj Shukla <rajshukla9630@gmail.com>
2026-04-13 23:49:31 +0530

    angular app setting paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { NotesComponent } from './notes/notes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        // lazy loading
+        loadComponent: () => import('../app/notes/notes.component').then(m=>m.NotesComponent)
+    }
+];
